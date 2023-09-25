c63136cf550fd71f81436f1d7840ad6746e0f25f
= <koraydincc@icloud.com>
2023-09-25 15:44:22 +0300

    Project
    Project

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,45 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { FilmContext } from '../App';
 import BackButon from '@mui/icons-material/ArrowBackIos';
 import NextButon from '@mui/icons-material/ArrowForwardIos';
 import MovieCard from '../components/MovieCard';
-import NavBar from '../components/NavBar';
-import { useState } from 'react';
+import NextIcon from '@mui/icons-material/ArrowForwardIos';
+import BackIcon from '@mui/icons-material/ArrowBackIos';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios'
 
 function HomePage() {
- 
-  const { movies, setPages, pages, setMovies, inputText } = useContext(FilmContext);
-  const [editorMovies, setEditorMovies] = useState([])
-  const [moviesPage, setMoviesPage] = useState(5)
+  const { movies, setPages, pages } = useContext(FilmContext);
+  const [editorMovies, setEditorMovies] = useState([]);
+  const [moviesPageNext, setMoviesPageNext] = useState(5);
+  const [moviesPageBack, setMoviesPageBack] = useState(0);
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTg2ZjU4NjZiZGUxYjI5NzczNzcyOWNjOWRmZGZmNSIsInN1YiI6IjY1MDAzNTc2MWJmMjY2MDBhYzc0YmY4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gxi6R_SScCq8l6cxJ0f6_N7TPqTgB4KJOMgSl-HJtzE'
+      }
+    };
+    const updateData = fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&vote_average.gte=8', options)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      const updatedEditorMovies = [...editorMovies, ...data.results];
+  
+    
+      setEditorMovies(updatedEditorMovies);
+    })
+    .catch(err => console.error(err));
+  
+
+     
+  }, []);
+  
+  
+    
+   
   
 
   const handleBackClick = () => {
@@ -20,72 +49,59 @@ function HomePage() {
       setPages(1);
     }
   }
-   
-  const editMovies = () => {
-    if (pages < 20) {
-      const bestEditorMovies = movies.filter((movie) => movie.vote_average > 7.5);
-      setEditorMovies([...editorMovies, ...bestEditorMovies]);
-      setPages(pages + 1);
-
-    } else {
-      return false
-    }
-  };
 
 
+  console.log(editorMovies)
 
+  
 
   const handleNextShowClick = () => {
-         setMoviesPage(moviesPage + 1)
+    setMoviesPageNext(moviesPageNext + 1);
+    setMoviesPageBack(moviesPageBack + 1);
+
   }
 
   const handleBackShowClick = () => {
-    if (moviesPage <= 5) {
-      setMoviesPage(6)
+    if (moviesPageNext <= 5) {
+      setMoviesPageNext(6);
+    } else {
+      setMoviesPageNext(moviesPageNext - 1);
+      setMoviesPageBack(moviesPageBack - 1);
     }
-    else{
-      setMoviesPage(moviesPage - 1)
-    }
-
-    console.log(moviesPage)
-   
   }
-    
-  useEffect(()=>{
-    editMovies()
-   
-  },[pages])
 
-  console.log(editorMovies)
-  
+ 
+
+
 
   return (
     <div>
       <div className='editor'>
-       <h4>Editörün Seçimi</h4>
-       <div className='editorMovie'>
-        <button onClick={handleBackShowClick}>Geri</button>
-         {editorMovies.slice(0,moviesPage).map((movie)=>{
+        <h4>Editörün Seçimi</h4>
+        <div className='editorMovie'>
+          <button className='editorBtn' disabled={moviesPageNext === 5} onClick={handleBackShowClick}><BackIcon></BackIcon></button>
+          {editorMovies.slice(moviesPageBack, moviesPageNext).map((movie) => {
             return (
-              <div>{movie.title}</div>
-            )
-         })}
-         <button onClick={handleNextShowClick}>İleri</button>
-       </div>
-    </div>
-
-      
-      
+              <div className='editorCard' key={movie.id}>
+                {movie.title}
+                <img
+                  className='img'
+                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                ></img>
+              </div>
+            );
+          })}
+          <button className='editorBtn' disabled={moviesPageNext === editorMovies.length} onClick={handleNextShowClick}><NextIcon></NextIcon></button>
+        </div>
+      </div>
       <div className="movieList">
         <MovieCard></MovieCard>
-      
       </div>
       <div className="actions">
-         <button onClick={handleBackClick}><BackButon /></button>
-         {pages}
-         <button onClick={() => setPages(pages + 1)}><NextButon /></button>
+        <button onClick={handleBackClick}><BackButon /></button>
+        {pages}
+        <button onClick={() => setPages(pages + 1)}><NextButon /></button>
       </div>
-  
     </div>
   );
 }
